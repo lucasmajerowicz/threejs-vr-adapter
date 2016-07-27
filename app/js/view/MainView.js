9a0b467e50e598a658cfc66c07a2b84bb5964004
Lucas Majerowicz <lucas.majerowicz@autodesk.com>
2016-07-28 01:30:40 +0300

    first commit!

---
+++ app/js/view/MainView.js
@@ -0,0 +1,53 @@
+export default class MainView {
+    constructor(controller, renderingContextFactory) {
+        this.controller = controller;
+        this.renderingContextFactory = renderingContextFactory;
+        this.renderingContext = this.createRenderingContext();
+    }
+
+    createRenderingContext() {
+        const domContainer = document.createElement('div');
+
+        document.body.appendChild(domContainer);
+
+        const renderingContext = this.renderingContextFactory.createRenderingContext(domContainer);
+
+        domContainer.appendChild(renderingContext.getDomElement());
+
+        return renderingContext;
+    }
+
+    initialize() {
+        window.addEventListener( 'resize', (e) => this.onWindowResize(), false );
+        this.renderingContext.addObserver( 'onControllerPositionChange', (e) => {
+            this.controller.onControllerMoved(e.controller);
+        });
+
+        this.render();
+    }
+
+    render() {
+        requestAnimationFrame(() => this.render());
+
+        this.renderingContext.onRender();
+    }
+
+    onWindowResize(){
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+
+        this.renderingContext.renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    get scene() {
+        return this.renderingContext.scene;
+    }
+
+    get camera() {
+        return this.renderingContext.camera;
+    }
+
+    get renderer() {
+        return this.renderingContext.renderer;
+    }
+}
